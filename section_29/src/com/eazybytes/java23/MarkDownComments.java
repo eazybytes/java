3ba07eaedf847a80da7127a8a1c2323bb2a7d48f
eazybytes <tutor@eazybytes.com>
2024-09-21 20:06:12 +0530

    Java 22 and 23 new features

---
+++ section_29/src/com/eazybytes/java23/MarkDownComments.java
@@ -0,0 +1,53 @@
+package com.eazybytes.java23;
+
+/// **This class contains methods for performing basic math operations**
+/// @author Eazybytes
+ /// @version 1.0
+public class MarkDownComments {
+
+
+    /// | Input | Output |
+    /// |-------|-------|
+    /// | 2,3     | 5 |
+    /// | 9,2     | 11  |
+    /// | 25,75     | 100 |
+    /// **This method adds two numbers**
+    /// @param a first number
+     /// @param b second number
+     /// @return sum of the two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    /// ## This method subtract two numbers
+    ///   - 5,3 = 2
+    ///   - 9,2 = 7
+    /// @param a first number
+     /// @param b second number
+     /// @return ***subtraction of the two numbers***
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    ///  ---
+    /// # This method multiply two numbers
+    /// @param a first number
+     /// @param b second number
+     /// @return ***multiplication of the two numbers***
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    /// # This method divide two numbers
+    /// ```
+     /// @Override
+     /// public void division() ...
+     /// ```
+    /// @param a first number
+     /// @param b second number
+     /// @return ***division of the two numbers***
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+}
